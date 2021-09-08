d4c4578dcf0f3d20d0df079069e63dce4d333f20
DamianEdwards <damian@damianedwards.com>
2021-09-07 19:38:02 -0700

    Fix failing tests
    Ensure the test database is properly initialized in a thread-safe manner as tests run in parallel.

--- tests/MinimalApiPlayground.Tests/PlaygroundApplication.cs
+++ tests/MinimalApiPlayground.Tests/PlaygroundApplication.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -7,6 +7,7 @@ namespace MinimalApiPlayground.Tests
 {
     internal class PlaygroundApplication : WebApplicationFactory<Program>
     {
+        private static readonly string _connectionString = "Data Source=testtodos.db";
         private readonly string _environment;
 
         public PlaygroundApplication(string environment = "Development")
@@ -16,6 +17,8 @@ namespace MinimalApiPlayground.Tests
 
         protected override IHost CreateHost(IHostBuilder builder)
         {
+            EnsureDb();
+
             builder.UseEnvironment(_environment);
 
             // Add mock/test services to the builder here
@@ -23,12 +26,33 @@ namespace MinimalApiPlayground.Tests
             {
                 services.AddScoped(sp =>
                 {
-                    // Replace SQL Lite with test DB
-                    return new SqliteConnection("Data Source=testtodos.db");
+                    // Replace SQLite connection string for tests
+                    return new DbContextOptionsBuilder<TodoDb>()
+                        .UseSqlite(_connectionString)
+                        .UseApplicationServiceProvider(sp)
+                        .Options;
                 });
             });
 
             return base.CreateHost(builder);
+        }
+
+        private static readonly Lazy<bool> _dbInit = new(() =>
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<TodoDb>()
+                            .UseSqlite(_connectionString)
+                            .Options;
+
+            using var db = new TodoDb(dbContextOptions);
+            db.Database.EnsureDeleted();
+            db.Database.Migrate();
+
+            return true;
+        });
+
+        private static void EnsureDb()
+        {
+            var _ = _dbInit.Value;
         }
     }
 }
